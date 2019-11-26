41093ca16092fad3f202d3ee6a6cc44b8b9326bd
Swaibu Rumbiiha <rumbiihas@gmail.com>
2019-11-26 16:53:03 +0200

    chore(setup-rdux):set redux for react app
    - setup redux
    - install redux and react redux
    - add basic redux code and structure
    
    [Finishes #168781741]

---
+++ src/Redux/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from './node_modules/redux';
+
+const allReducers = combineReducers();
+
+export default allReducers;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import ReactDOM from 'react-dom';
 import App from './App/App.js';
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from './Redux/store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
