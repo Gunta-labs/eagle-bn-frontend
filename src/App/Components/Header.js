a8a6181bfb3bf1e39a26698df286119572e6e02d
Swaibu Rumbiiha <rumbiihas@gmail.com>
2019-12-10 18:45:10 +0200

    chore(landing -page):add user pannel  boilerplate

--- src/App/Components/Header.js
+++ src/App/Components/Header.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import avatar from '../../Assets/images/bob.jpg';
+import { faUser, faComment, faFileAlt, faClipboard } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Header() {
 	return (
@@ -10,48 +13,68 @@ function Header() {
 				<meta http-equiv='X-UA-Compatible' content='ie=edge' />
 				<title>Barefoot-Nomad</title>
 			</head>
-			<header>
-				<nav className='navbar navbar-expand-md navbar-light fixed-top bg-transparent shadow-sm'>
-					<a className='navbar-brand text-primary bold' href='/'>
-						<h4 className='logo-txt'>Barefoot-Nomad</h4>
-					</a>
-					<button
-						className='navbar-toggler collapsed'
-						type='button'
-						data-toggle='collapse'
-						data-target='#navbarCollapse'
-						aria-controls='navbarCollapse'
-						aria-expanded='false'
-						aria-label='Toggle navigation'
-					>
-						<span className='navbar-toggler-icon' />
-					</button>
-					<div className='ml-auto navbar-collapse collapse' id='navbarCollapse'>
-						<ul className='navbar-nav ml-auto'>
-							<li className='nav-item active'>
-								<a className='nav-link mx-3 ' href='/'>
-									Home <span className='sr-only'>(current)</span>
-								</a>
-							</li>
-							<li className='nav-item mx-3'>
-								<a className='nav-link' href='/'>
-									Acommodations
-								</a>
-							</li>
-							<li className='nav-item mx-3'>
-								<a className='nav-link mx-3' href='/signup'>
-									Signup
-								</a>
-							</li>
-							<li className='nav-item mx-3'>
-								<a className='nav-link btn btn-primary btn-sm px-5' href='/login'>
-									Login
-								</a>
-							</li>
-						</ul>
-					</div>
-				</nav>
-			</header>
+			<nav class='navbar navbar-expand-lg bg-light fixed-top shadow-sm'>
+				<a class='navbar-brand' href='/'>
+					Sidebar Nav
+				</a>
+				<button
+					class='navbar-toggler'
+					type='button'
+					data-toggle='collapse'
+					data-target='#navbarCollapse'
+					aria-controls='navbarCollapse'
+					aria-expanded='false'
+					aria-label='Toggle navigation'
+				>
+					<span class='navbar-toggler-icon'></span>
+				</button>
+
+				<div class='collapse navbar-collapse' id='navbarCollapse'>
+					<ul class='navbar-nav mr-auto sidenav shadow-sm' id='navAccordion'>
+						<li class='nav-item active user-nav shadow-sm'>
+							<div className='d-flex flex-column ml-5 mt-3'>
+								<img class='avatar' src={avatar} alt='user' />
+								<h6 className='py-2 pl-1'>my name</h6>
+							</div>
+						</li>
+						<li class='nav-item m-2'>
+							<a class='nav-link text-secondary' href='/'>
+								<FontAwesomeIcon icon={faUser} />
+								<span className='ml-3'>My account</span>
+							</a>
+						</li>
+						<li class='nav-item m-2'>
+							<a class='nav-link' href='/'>
+								<FontAwesomeIcon icon={faClipboard} />
+								<span className='ml-3'>Trips requests</span>
+							</a>
+						</li>
+						<li class='nav-item m-2 '>
+							<a class='nav-link' href='/'>
+								<FontAwesomeIcon icon={faComment} />
+								<span className='ml-3'>Comments</span>
+							</a>
+						</li>
+						<li class='nav-item m-2'>
+							<a class='nav-link' href='/'>
+								<FontAwesomeIcon icon={faFileAlt} />
+								<span className='ml-3'>History</span>
+							</a>
+						</li>
+					</ul>
+					<form class='form-inline ml-auto mt-2 mt-md-0'>
+						<input
+							class='form-control mr-sm-2'
+							type='text'
+							placeholder='Search'
+							aria-label='Search'
+						/>
+						<button class='btn btn-outline-success my-2 my-sm-0' type='submit'>
+							Search
+						</button>
+					</form>
+				</div>
+			</nav>
 		</>
 	);
 }
