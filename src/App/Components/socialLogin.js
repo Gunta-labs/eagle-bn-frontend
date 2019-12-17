b154450c64168e2d7024b92d01d58dc54088df99
alexismajya <48722615+alexismajya@users.noreply.github.com>
2019-12-17 14:25:25 +0200

    Create socialLogin.js

---
+++ src/App/Components/socialLogin.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import googleIcon from '../../Assets/images/icon_google.svg';
+import Button from './socialButton';
+import BaseUrl from '../../Api/config';
+import LoginFacebook from './facebookLogin';
+
+class SocialLogin extends React.Component {
+	GoogleLogin = () => {
+		window.location.assign(`${BaseUrl}/users/google`);
+	};
+	render() {
+		return (
+			<div className='social'>
+				<div>
+					<LoginFacebook />
+				</div>
+				<div>
+					<Button
+						name='Google'
+						logo={googleIcon}
+						styleClass='btn btn-google Goo'
+						onClick={this.GoogleLogin}
+					/>
+				</div>
+			</div>
+		);
+	}
+}
+export default SocialLogin;
