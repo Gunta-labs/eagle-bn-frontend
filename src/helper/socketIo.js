a0402499eec5886dcf5ebb19b07b81773ef21e80
lemoissonM <murhulametre@gmail.com>
2020-01-13 17:43:54 +0200

    chore(real-time-comments) add real time comments capabilities - add toast on new comment - insert new comment in comment list [finishes #170674437]

--- src/helper/socketIo.js
+++ src/helper/socketIo.js
@@ -5,6 +5,7 @@ import store from '../Redux/store';
 import constant from '../Redux/constants';
 import { toast } from 'react-toastify';
 import user from './helper';
+import { Redirect } from 'react-router-dom';
 
 export const initializeSocketIo = token => {
 	const socket = socketClient(url, { query: { token } });
@@ -53,5 +54,24 @@ export const initializeSocketIo = token => {
 			toast(<Msg />);
 		}
 		store.dispatch({ type: constant.CHAT_NEW_MESSAGE });
+	});
+	socket.on('new_comment', data => {
+		const Msg = ({ closeToast }) => (
+			<div
+				className='alert alert-dismissible'
+				onClick={e => (window.location = `/requests/${data.modelId}`)}
+			>
+				{'New comment from ' + data.description}
+				<button type='button' className='close'>
+					<span> {closeToast} </span>
+				</button>
+			</div>
+		);
+		toast(Msg);
+		store.dispatch({
+			type: constant.NEW_NOTIFCATION,
+			notification: data,
+		});
+		store.dispatch({ type: constant.NEW_COMMENT, payload: data.data });
 	});
 };
